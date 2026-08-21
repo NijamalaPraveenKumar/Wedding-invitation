03a0b3b9d80f0784d8d13f48688eeadee6acb8b6
Praveen kumar <praveenkumarnijamala@gmail.com>
2026-08-21 14:21:44 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Wedding-invitation/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
